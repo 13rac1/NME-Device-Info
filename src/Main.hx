8240844da87665bccb180afcfb4d09ef4eae6892
Brad Erickson <eosrei@gmail.com>
2012-11-02 12:07:04 -0700

    Add Haptic

--- src/Main.hx
+++ src/Main.hx
@@ -6,16 +6,30 @@ import nme.text.TextField;
 import nme.text.TextFormat;
 import nme.text.TextFormatAlign;
 import nme.events.Event;
+import nme.events.MouseEvent;
 import nme.Lib;
 import nme.sensors.Accelerometer;
 import nme.events.AccelerometerEvent;
+import nme.feedback.Haptic;
 import nme.ui.Multitouch;
 import nme.ui.MultitouchInputMode;
+import nme.system.Capabilities;
+import nme.system.System;
+import nme.text.Font; //enumerateFonts
+
+
 /**
  * NME/Haxe device environment information
  * @author Brad Erickson
  */
 
+/**
+ * nme.system.System.deviceID
+ * nme.system.System.totalMemory
+ * http://www.haxenme.org/api/types/nme/system/Capabilities.html
+ * http://www.haxenme.org/api/types/nme/filesystem/File.html
+ * nme.Lib.getTimer()
+ */
 class Main extends Sprite {
 	private var txtAccel:TextField;
 	private var txtRes:TextField;
@@ -49,6 +63,8 @@ class Main extends Sprite {
 		txtTitleFormat.align = TextFormatAlign.CENTER;
 		txtTitle.defaultTextFormat = txtTitleFormat;
 		txtTitle.text = "NME/Haxe device environment information";
+		txtTitle.text = Std.string(System.deviceID);// Std.string(Capabilities.screenDPI);
+		
 		addChild (txtTitle);
 		
 		txtAccel = new TextField();
@@ -64,9 +80,36 @@ class Main extends Sprite {
 		txtRes.width = 1024;
 		txtRes.defaultTextFormat = defaultFormat;
 		addChild (txtRes);
+		
+		var button:Sprite = new Sprite();
+		button.graphics.beginFill(0xffffff);
+		button.graphics.drawRoundRect(10, 100, 100, 50, 10, 10);
+		button.graphics.endFill();
+		button.addEventListener(MouseEvent.CLICK, button1_onclick);
+		addChild(button);
+
+		var button2:Sprite = new Sprite();
+		button2.graphics.beginFill(0xffffff);
+		button2.graphics.drawRoundRect(120, 100, 100, 50, 10, 10);
+		button2.graphics.endFill();
+		button2.addEventListener(MouseEvent.CLICK, button2_onclick);
+		addChild(button2);
+		
 
 	}
-	
+
+	private function button1_onclick(event:MouseEvent):Void {
+		#if cpp
+		Haptic.vibrate(0, 1000);
+		#end
+	}
+
+	private function button2_onclick(event:MouseEvent):Void {
+		#if cpp
+		Haptic.vibrate(10, 1000);
+		#end
+	}
+
 	private function setupEvents():Void {
 		if (Accelerometer.isSupported) {
 			var accl:Accelerometer = new Accelerometer();
@@ -75,6 +118,11 @@ class Main extends Sprite {
 		else {
 			txtAccel.text = "Accelerometer is not supported ";
 		}
+	}
+	
+	private var fields:Array<Dynamic>;
+	
+	private function addField:TextField {
 		
 	}
 	
